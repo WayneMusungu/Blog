1cf5fc19a908e6bf5abec705ec02929fbe51e4e4
wayne <waynemuyera17@gmail.com>
2024-11-05 09:16:40 +0300

    User Posts API View Test

--- posts/tests/test_posts_views.py
+++ posts/tests/test_posts_views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from authentication.models import User
 from posts.models import Post, Category, Comment
 
-class UserPostsAPIView(APITestCase):
+class PostsAPIViewTest(APITestCase):
     def setUp(self):
         # Create a user and a post
         self.user = User.objects.create_user(
@@ -85,3 +85,79 @@ class UserPostsAPIView(APITestCase):
             result,
             [{'title': 'Introduction to Django', 'author__email': 'janedoe@test.com'}]
         )
+
+
+class UserPostsAPIViewTest(APITestCase):
+    def setUp(self):
+        # Create a user and a post
+        self.user_one = User.objects.create_user(
+            email='wallace@gmail.com',
+            first_name='Wallace',
+            last_name='Smith',
+            username='wallsmith',
+            password='password123',
+        )
+        self.user_two = User.objects.create_user(
+            email='arthur@yahoo.com',
+            first_name='Arthur',
+            last_name='White',
+            username='aWhite',
+            password='password123',
+        )
+        self.user_three = User.objects.create_user(
+            email='whitney@yahoo.com',
+            first_name='Yuston',
+            last_name='Whitney',
+            username='whytney',
+            password='password123',
+        )
+        self.post = Post.objects.create(
+            title='Javascript',
+            body='100 days of Javascript',
+            author=self.user_one,
+        )
+        self.category_one = Category.objects.create(
+            name='Javascript',
+        )
+        self.category_two = Category.objects.create(
+            name='Programming',
+        )
+        self.post.categories.add(self.category_one, self.category_two)
+        self.comment = Comment.objects.create(
+            post = self.post,
+            author = self.user_two,
+            content = 'This is looks fun joining you'
+        )    
+        self.comment_2 = Comment.objects.create(
+            post = self.post,
+            author = self.user_three,
+            content = 'I want to get my JS fundamental right!!'
+        )
+        
+    def tearDown(self):
+        Comment.objects.all().delete()
+        Post.objects.all().delete()
+        Category.objects.all().delete()
+        User.objects.all().delete()
+        
+    def test_user_list_posts(self):
+        url = reverse('user-post-list-create')
+        self.client.force_authenticate(user=self.user_one)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user_one.user_posts.count(), 1)
+        
+    def test_user_create_post(self):
+        url = reverse('user-post-list-create') 
+        self.client.force_authenticate(user=self.user_one)
+        data = {
+                "title": "Getting Started with Python for Machine Learning",
+                "body": "This article introduces the essential tools and libraries for machine learning with Python. We'll cover how to use libraries like Scikit-learn for building and evaluating models, TensorFlow and PyTorch for deep learning, and Pandas for preprocessing data. Additionally, we’ll explore techniques for splitting data into training and testing sets, selecting the right algorithms, and tuning hyperparameters to improve model performance.",
+                "categories": [
+                    {"name": "Machine Learning"},
+                    {"name": "Python"}
+                ]
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.user_one.user_posts.count(), 2)
